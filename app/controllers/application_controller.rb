a155916f1793b8bf3c8d7ee9728eeb3e3d79fe00
Tenzin Jorden <tenden96@gmail.com>
2022-03-07 20:17:17 -0800

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,27 @@ class ApplicationController < Sinatra::Base
     { message: "Hello world" }.to_json
   end
 
+  get '/games' do
+    # get all the games from the database
+    games = Game.all
+    # games = Game.all.order(:title) returns only games title 
+    # games = Game.all.limit(5) returns first 5 games
+    # return a JSON response with an array of all the game data
+    games.to_json
+  end
+
+  get '/games/:id' do
+    # look up the game in the database using its ID
+    game = Game.find(params[:id])
+    # send a JSON-formatted response of the game data
+    game.to_json
+  end
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+
+    # display reviews in the JSON response
+    game.to_json(include: { reviews: { include: :user } })
+  end
+
 end
